4ec2656288b43dd4289306f4a7305d8eddf1e5c0
Saidul Badhon <31236650+SaidulBadhon@users.noreply.github.com>
2024-10-06 19:56:28 +0600

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ export default function Footer() {
 
   return (
     <footer
-      className="pt-[150px] dark:bg-gray-900 container flex flex-col items-center justify-center p-6 mx-auto space-y-4 sm:flex-row"
+      className="pt-[150px]  container flex flex-col items-center justify-center p-6 mx-auto space-y-4 sm:flex-row"
       style={{
         zIndex: 1000,
       }}
     >
-      <p className="text-sm text-gray-600 dark:text-gray-300 p-0 !m-0">
+      <p className="text-sm text-gray-600 p-0 !m-0">
         © {currentYear} SaidulBadhon. All rights reserved.
       </p>
     </footer>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -65,7 +65,7 @@ export default function HomePage() {
               </Link>
 
               <a
-                className="bg-white ring-1 p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                className="bg-white ring-1 p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack"
                 href="https://www.linkedin.com/in/saidulbadhon"
                 target="_blank"
               >
@@ -73,7 +73,7 @@ export default function HomePage() {
               </a>
 
               <a
-                className="bg-white ring-1 p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                className="bg-white ring-1 p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
                 href="https://github.com/SaidulBadhon"
                 target="_blank"
               >
